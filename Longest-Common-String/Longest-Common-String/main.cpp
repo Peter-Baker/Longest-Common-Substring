9fcfd19de3f1d49baaf352d8bd78ab186d3acc2a
Peter Baker <peter.baker@snhu.edu>
2020-11-09 10:42:18 -0500

    Created C++ Project w/ main.cpp

---
+++ Longest-Common-String/Longest-Common-String/main.cpp
@@ -0,0 +1,16 @@
+/************************
+Program:  main.cpp
+Purpose: Given two strings 'X' and 'Y', find the length of the longest common substring.
+Author:  Peter Baker, Jacob Burton, and Kaven Arango
+Date:    Nov 19, 2020
+*********************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+	return 0;
+}
